2c6844ca36f82814edf0dd3866c401eeeaec5067
Pedro Comesaña <Pedro.Alonso@softwareone.com>
2023-01-12 10:43:18 +0100

    first commit

---
+++ .Net/Ejercicio2/Ejercicio2/Controllers/EmpleadoController.cs
@@ -0,0 +1,31 @@
+﻿using Ejercicio2.Config;
+using Ejercicio2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejercicio2.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly DBEmpleadoContext _context;
+        public EmpleadoController(DBEmpleadoContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<List<Empleado>>> Get() 
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+        [HttpPost("save")]
+        public async Task<ActionResult> Post(Empleado empleado)
+        {
+            _context.Add(empleado); // Añadir al contexto el empleado
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    
+    }
+}
---
+++ .Net/Ejercicio2/Ejercicio2/Models/Empleado.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio2.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Name { get; set;}
+    }
+}
